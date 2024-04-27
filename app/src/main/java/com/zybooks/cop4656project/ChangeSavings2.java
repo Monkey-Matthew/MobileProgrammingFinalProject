0624e17026af6fd69e9a367d086281af5ceff95f
Matthew-Gonzales <monkeymatthew0217@gmail.com>
2024-04-27 19:09:50 -0400

    Added the change savings amount button with more java classes and mxl files.

---
+++ app/src/main/java/com/zybooks/cop4656project/ChangeSavings2.java
@@ -0,0 +1,28 @@
+package com.zybooks.cop4656project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChangeSavings2 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_savings1goal);
+
+        Button backButton = findViewById(R.id.button_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the current activity and go back to the previous activity
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/zybooks/cop4656project/ChangeSavingsGoal.java
+++ app/src/main/java/com/zybooks/cop4656project/ChangeSavingsGoal.java
@@ -7,33 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SettingsActivity extends AppCompatActivity {
+public class ChangeSavingsGoal extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.change_savegoal);
 
-        // Find the button in settings_activity.xml
-        Button openChangeBudgetButton = findViewById(R.id.changeBudgetButton);
-        Button openChangeSavingButton = findViewById(R.id.button_change_save_goal);
+        Button savings1button = findViewById(R.id.savings1button);
+        Button savings2button = findViewById(R.id.savings2button);
         Button backButton = findViewById(R.id.button_back);
 
-        // Set OnClickListener for the button
-        openChangeBudgetButton.setOnClickListener(new View.OnClickListener() {
+        savings1button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create an intent to start the activity to display change_budget.xml
-                Intent intent = new Intent(SettingsActivity.this, ChangeBudget.class);
+                Intent intent = new Intent(ChangeSavingsGoal.this, ChangeSavings1.class);
                 startActivity(intent);
             }
         });
 
-        openChangeSavingButton.setOnClickListener(new View.OnClickListener() {
+        savings2button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create an intent to start the activity to display change_budget.xml
-                Intent intent = new Intent(SettingsActivity.this, ChangeSavingsGoal.class);
+                Intent intent = new Intent(ChangeSavingsGoal.this, ChangeSavings2.class);
                 startActivity(intent);
             }
         });
